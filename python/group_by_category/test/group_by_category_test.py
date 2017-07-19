bd580d2885a2d634af95f130ced2647c54be2524
Bryan Yang <kenshin2004528@gmail.com>
2017-07-19 14:35:58 +0000

    add group by category

---
+++ python/group_by_category/test/group_by_category_test.py
@@ -0,0 +1,48 @@
+"""Simple test example"""
+
+import unittest2
+from sparktestingbase.testcase import SparkTestingBaseTestCase
+from ..solution import answer
+from lib.tools.timeout import *
+
+class GroupByCategoryTest(SparkTestingBaseTestCase):
+    """Simple hell world example test."""
+
+    def test_group_by_category(self):
+        """Test a parallelize & collect."""
+        input = [('apple', 'fruit'),
+                ('apple', 'fruit'),
+                ('banana', 'fruit'),
+                ('mac', '3c'),
+                ('ipad', '3c'),
+                ('ipad', '3c'),
+                ('ipad', '3c'),]
+
+        rdd = self.sc.parallelize(input)
+        timeout_ = timeout(answer, 5)
+        result = timeout_(rdd)
+        expected = self.sc.parallelize([('3c','ipad', 3), ('3c', 'mac', 1), ('fruit', 'apple', 2), ('fruit', 'banana', 1)])
+        print(result.collect())
+        self.assertTrue(self.assertRDDEqualsWithOrder(expected, result))
+
+    def test_group_by_category_with_order(self):
+        """Test a parallelize & collect."""
+        input = [('mac', '3c'),
+                ('apple', 'fruit'),
+                ('apple', 'fruit'),
+                ('mac','3c'),
+                ('banana', 'fruit'),
+                ('mac', '3c'),
+                ('ipad', '3c'),
+                ('ipad', '3c')]
+
+        rdd = self.sc.parallelize(input)
+        timeout_ = timeout(answer, 5)
+        result = timeout_(rdd)
+        expected = self.sc.parallelize([('3c', 'mac', 3), ('3c', 'ipad', 2), ('fruit', 'apple', 2), ('fruit', 'banana', 1)])
+        print(result.collect())
+        self.assertTrue(self.assertRDDEqualsWithOrder(expected, result))
+
+   
+if __name__ == "__main__":
+    unittest2.main()
