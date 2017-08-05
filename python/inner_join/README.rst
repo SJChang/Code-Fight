04a20ec0c8e3ba09d6f091e425a8bea156850f34
bryanyang0528 <kenshin2004528@gmail.com>
2017-08-05 13:05:15 +0000

    add innerjoin

---
+++ python/inner_join/README.rst
@@ -0,0 +1,13 @@
+# Inner Join
+
+**Input**: 
+
+rddA = `sc.parallelize([('fruit','apple'), ('fruit','apple'), ('fruit','banana'), ('fruit','mac')])`
+
+rddB = `sc.parallelize([('apple', 5), ('banana', 3), ('kiwi', 10)])`   
+ 
+**Output**: 
+
+`[('apple', 'fruit', 5), ('apple', 'fruit', 5), ('banana', 'fruit', 3)]` (RDD)
+
+
---
+++ python/inner_join/test/inner_join_test.py
@@ -0,0 +1,31 @@
+"""Simple test example"""
+
+import unittest2
+from sparktestingbase.testcase import SparkTestingBaseTestCase
+from ..solution import answer
+from lib.tools.timeout import *
+
+class CountByCategoryTest(SparkTestingBaseTestCase):
+    """Simple hell world example test."""
+
+    def test_basic_join(self):
+        inputA = [('fruit', 'apple'),
+                ('fruit','apple'),
+                ('fruit','banana'),
+                ('3c', 'mac')]
+
+        inputB = [('apple', 5), 
+                ('banana', 3), 
+                ('kiwi', 10)]
+
+        rddA = self.sc.parallelize(inputA)
+        rddB = self.sc.parallelize(inputB)
+        timeout_ = timeout(answer, 5)
+        result = timeout_(rddA, rddB)
+        expected = self.sc.parallelize([('apple', ('fruit', 5)), 
+                                        ('apple', ('fruit', 5)), 
+                                        ('banana', ('fruit', 3))])
+        self.assertTrue(self.assertRDDEquals(expected, result))
+
+if __name__ == "__main__":
+    unittest2.main()
