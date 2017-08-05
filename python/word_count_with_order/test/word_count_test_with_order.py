ce9967a5baad79d0209d21ffa692bdbe1dede0a2
bryanyang0528 <kenshin2004528@gmail.com>
2017-08-05 11:49:11 +0000

    add word_count_with_order

---
+++ python/word_count_with_order/test/word_count_test_with_order.py
@@ -0,0 +1,37 @@
+"""Simple test example"""
+
+import unittest2
+from sparktestingbase.testcase import SparkTestingBaseTestCase
+from ..solution import answer
+from lib.tools.timeout import *
+
+class WordCountTest(SparkTestingBaseTestCase):
+    """Simple hell world example test."""
+
+    def test_order_by_key(self):
+        """Test a parallelize & collect."""
+        input = ["hello world"]
+        rdd = self.sc.parallelize(input)
+        timeout_ = timeout(answer, 5)
+        result = timeout_(rdd)
+        expected = self.sc.parallelize([('hello', 1), ('world',1)])
+        self.assertRDDEqualsWithOrder(expected, result)
+    
+    def test_order_by_value(self):
+        input = ["hello world world"]
+        rdd = self.sc.parallelize(input)
+        timeout_ = timeout(answer, 5)
+        result = timeout_(rdd)
+        expected = self.sc.parallelize([('world', 2), ('hello',1)])
+        self.assertRDDEqualsWithOrder(expected, result)
+
+    def test_same_value(self):
+        input = ["hello hello world world"]
+        rdd = self.sc.parallelize(input)
+        timeout_ = timeout(answer, 5)
+        result = timeout_(rdd)
+        expected = self.sc.parallelize([('hello', 2), ('world',2)])
+        self.assertRDDEqualsWithOrder(expected, result)
+
+if __name__ == "__main__":
+    unittest2.main()
